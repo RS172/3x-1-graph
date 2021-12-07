a4ca5c628fd683bdb22368ecc05c0800c1afb21d
RS172 <91712490+RS172@users.noreply.github.com>
2021-12-07 16:04:51 -0300

    Create 3x+1.py

---
+++ 3x+1.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+valor_apl=input("Qual o número que será aplicado?\n")
+valor_apl=int(valor_apl)
+valor_ger=0
+
+valor=[]
+valorb=[]
+
+while valor_apl !=1:
+
+  if valor_ger==0:
+    valor.append(valor_apl)
+    valor_ger+=1
+    print("valores: ")
+
+  if valor_apl==1:
+    print("1.0(even)")
+
+  if valor_apl%2==0:
+    print (str(valor_apl)+" (even)")
+    valor_apl=(valor_apl/2)
+    valor.append(valor_apl)
+
+  else:
+    print (str(valor_apl)+" (odd)")
+    valor_apl=valor_apl*3+1
+    valor.append(valor_apl)
+
+  valor_ger+=1
+
+print ("Chegou em 1 após "+str(valor_ger)+" gerações")
+
+for x in range(valor_ger):
+  valorb.append(x)
+
+plt.scatter(valorb,valor)
