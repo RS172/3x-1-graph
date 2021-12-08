f38773bc835510bdb3bfecd868839c92c6e2c222
RS172 <91712490+RS172@users.noreply.github.com>
2021-12-07 21:50:39 -0300

    Update 3x+1.py

--- 3x+1.py
+++ 3x+1.py
@@ -14,21 +14,21 @@ while valor_apl !=1:
     valor_ger+=1
     print("valores: ")
 
-  if valor_apl==1:
-    print("1.0(even)")
+
 
   if valor_apl%2==0:
-    print (str(valor_apl)+" (even)")
+    print (str(valor_apl))
     valor_apl=(valor_apl/2)
     valor.append(valor_apl)
 
   else:
-    print (str(valor_apl)+" (odd)")
+    print (str(valor_apl))
     valor_apl=valor_apl*3+1
     valor.append(valor_apl)
 
   valor_ger+=1
 
+print("1.0")
 print ("Chegou em 1 após "+str(valor_ger)+" gerações")
 
 for x in range(valor_ger):
